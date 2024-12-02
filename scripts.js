076fa3b549586775835408ef4e608a2716cf171d
Abhinavaa-S-Kumar <97662274+Abhinavaa-S-Kumar@users.noreply.github.com>
2024-12-02 19:25:42 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function showButtons() {
+    // Reveal the button section
+    const buttonSection = document.getElementById('button-section');
+    buttonSection.classList.remove('hidden');
+    buttonSection.scrollIntoView({ behavior: 'smooth' });
+}
